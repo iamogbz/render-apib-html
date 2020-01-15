c6d92f669d70c4b13cf52a98ba99501cdaa2379d
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-01-14 22:18:37 -0500

    fix: return updated cf event response

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
 import { CloudFrontResponseEvent, CloudFrontResultResponse } from "aws-lambda";
 
-const encoding = "base64";
+const encoding: "base64" = "base64";
 const atob = (data: string): string => Buffer.from(data, encoding).toString();
 const btoa = (data: string): string => Buffer.from(data).toString(encoding);
 
 export const handler = async (
-    event: CloudFrontResponseEvent,
+    event: DeepPartial<CloudFrontResponseEvent>,
 ): Promise<CloudFrontResultResponse> => {
     // eslint-disable-next-line no-console
     console.log(JSON.stringify(event));
-    return {
-        body: atob(btoa("Hello world!")),
+    const response = event.Records[0].cf.response;
+    const headers = Object.assign(
+        response.headers || {},
+        ...[
+            ["Access-Control-Allow-Headers", "X-Blueprint"],
+            ["Access-Control-Allow-Methods", "GET, HEAD, OPTIONS"],
+            ["Access-Control-Allow-Origin", "*"],
+        ].map(([key, value]) => ({ [key.toLowerCase()]: [{ key, value }] })),
+    );
+
+    const request = event.Records[0].cf.request;
+    if (!request.headers["x-blueprint"]) {
+        return Object.assign(response, {
+            body: "No api blueprint supplied",
+            headers,
+            status: "200",
+        });
+    }
+
+    const apib = atob(request.headers["x-blueprint"][0].value);
+    const html = `<html><body><pre>${apib}</pre></body><html>`;
+
+    return Object.assign(response, {
+        body: btoa(html),
+        bodyEncoding: encoding,
+        headers,
         status: "200",
-    };
+    });
 };
