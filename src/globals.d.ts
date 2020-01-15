9a9d83188882066e72d1633527adff59d29a80a7
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-01-15 09:21:46 -0500

    chore: rename type to globals

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,3 +1,7 @@
+type DeepPartial<T> = T extends object
+    ? { [K in keyof T]?: DeepPartial<T[K]> }
+    : T;
+
 declare module "fs-monkey";
 declare module "aglio";
 declare module "aglio-theme-olio";
--- src/types.d.ts
+++
@@ -1,3 +0,0 @@
-type DeepPartial<T> = T extends object
-    ? { [K in keyof T]?: DeepPartial<T[K]> }
-    : T;
