69f395452b1e6c71d2540208686eff09cd761d10
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-01-15 07:59:01 -0500

    chore: make deploy commands

--- plugins/deploy-lambda/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { prepare } = require("./prepare");
-const { publish } = require("./publish");
-const { verifyConditions } = require("./verifyConditions");
-
-module.exports = { prepare, publish, verifyConditions };
